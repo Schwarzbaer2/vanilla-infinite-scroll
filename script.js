6a32851a6f56453cddd7be752ab6e463f23cf0ed
Andreas Schwarz <andreas.schwarz20@posteo.de>
2021-03-19 17:22:12 +0100

    Adding fetch function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hallo Welt')
+// Unsplash API
+
+const count = 10;
+const apiKey = 'MfaorveAMJzltIi5zbl6GtWCCoAsEjOdQ43Ukz7u2c4';
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+// Get photos from Unsplash API
+
+async function getPhotos() {
+    try {
+        const respone = await fetch(apiURL);
+        const data = await respone.json();
+        console.log(data);
+    } catch (error) {
+    // Catch error
+}
+}
+
+
+// On Load
+getPhotos();
